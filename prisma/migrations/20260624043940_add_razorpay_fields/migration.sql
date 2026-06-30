ab19509c7a65a95bcea0622af275a7c3ad14b6e0
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2026-06-30 11:19:17 +0530

    Add Razorpay fields to Transaction model and update migration script

--- prisma/migrations/20260624043940_add_razorpay_fields/migration.sql
+++ prisma/migrations/20260624043940_add_razorpay_fields/migration.sql
@@ -1,6 +1,7 @@
 -- AlterTable
-ALTER TABLE `transaction` ADD COLUMN `gatewayResponse` TEXT NULL,
-    ADD COLUMN `paymentMethod` VARCHAR(191) NULL,
-    ADD COLUMN `razorpayOrderId` VARCHAR(191) NULL,
-    ADD COLUMN `razorpayPaymentId` VARCHAR(191) NULL,
-    ADD COLUMN `razorpaySignature` VARCHAR(191) NULL;
+ALTER TABLE `Transaction`
+ADD COLUMN `gatewayResponse` TEXT NULL,
+ADD COLUMN `paymentMethod` VARCHAR(191) NULL,
+ADD COLUMN `razorpayOrderId` VARCHAR(191) NULL,
+ADD COLUMN `razorpayPaymentId` VARCHAR(191) NULL,
+ADD COLUMN `razorpaySignature` VARCHAR(191) NULL;
